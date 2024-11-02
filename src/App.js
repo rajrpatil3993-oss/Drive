7c24ee6c8eeffb39b43e9ad2e8ad9f76a7d2e422
PGZON <prathameshgadgil2005@gmail.com>
2024-11-02 11:49:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="link-box">
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://drive.google.com/drive/folders/1IXxbMOwnqnXvhLXb9RWxABt4bqBOMa2_?usp=sharing"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Click Me
         </a>
-      </header>
+      </div>
     </div>
   );
 }
